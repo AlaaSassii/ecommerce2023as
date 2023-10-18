1842e2529f971d22a9d339d75464a279a4f767bc
alaasassii <alaasassijs@gmail.com>
2023-10-18 22:05:51 +0100

    add new things

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,2 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { productsCart } from "../types/productsCart";
+import type { PayloadAction } from '@reduxjs/toolkit'
 
+type initialStateType = {
+    userCartProduct: productsCart
+    totalPrice: number
+}
+
+const initialState: initialStateType = {
+    userCartProduct: [],
+    totalPrice: 0
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        incrementProductAmount: (state, action: PayloadAction<number>) => {
+            state.userCartProduct = state.userCartProduct.map((product) => {
+                if (product.id === action.payload) return { ...product, amount: product.amount + 1 }
+                return product
+            })
+        }
+    }
+})
