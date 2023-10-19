415c840f56a4ead7674d3948ed0fd4548fdc2cbf
alaasassii <alaasassijs@gmail.com>
2023-10-19 14:21:24 +0100

    add new things

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,9 @@ import { FC } from 'react'
 import Stars from '../Stars'
 import './ProductCard.scss'
 import { AiFillHeart } from 'react-icons/ai'
+import { useAppSelector } from '../../hooks/useAppSelector'
+import { useProductActions } from '../../hooks/useProductActions'
+import { singleProduct } from '../../types/singleProduct'
 type productCardType = {
     id: number,
     image: string,
@@ -9,9 +12,14 @@ type productCardType = {
     title: string,
     price: number,
     rateCount: number
-    category: string
+    category: string,
+    product: singleProduct
 }
-const ProductCard: FC<productCardType> = ({ id, image, rate, title, price, rateCount, category }) => {
+const ProductCard: FC<productCardType> = ({ id, image, rate, title, price, rateCount, category, product }) => {
+    const { userCartProduct } = useAppSelector(state => state.userCart)
+    const { deleteProduct, getProduct } = useProductActions()
+    const productInCart = userCartProduct.find(product => product.id === id)
+
     return (
         <div className='product__cart'>
             <div className="image">
@@ -22,10 +30,13 @@ const ProductCard: FC<productCardType> = ({ id, image, rate, title, price, rateC
                 <p><b>Price</b>{price}$</p>
                 <Stars fontSize='small' rate={rate} />
                 <p><b>Category:</b> {category} </p>
-                <button>Add to Cart</button>
+                <button onClick={() => { productInCart ? deleteProduct(id) : getProduct(product) }}>
+                    {productInCart ? "Remove from Cart" : "Add to Cart"}
+                </button>
             </div>
             <div className="rates">
                 <div className='icon'><AiFillHeart /></div>
+                <div className='data'>{rateCount}</div>
             </div>
         </div>
     )
