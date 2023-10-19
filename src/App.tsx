9c5a8ec3f461e5e48935b54c2772011b00455018
alaasassii <alaasassijs@gmail.com>
2023-10-19 19:40:33 +0100

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
-import { useAppDispatch } from "./hooks/useAppDispatch"
-import { useAppSelector } from "./hooks/useAppSelector"
-import useGetAllProducts from "./hooks/useGetAllProducts"
-import { changeSearchProductName } from "./redux/ProductsSlice"
-import { getNewProduct, removeProduct, incrementProductAmount, decrementProductAmount } from "./redux/cartSlice"
-import Tooltip from "./components/Tooltip"
+import { Route, Routes } from 'react-router-dom'
+
 const App = () => {
-  const { error, loading, products, searchProductName } = useGetAllProducts()
-  const dispatch = useAppDispatch();
-  const { userCartProduct } = useAppSelector(state => state.userCart)
   return (
-
-    <div>
-      search
-      <div className="tooltip_element">
-        <h1>Products fetch</h1>
-        <Tooltip message={`fetch`} position="bottom" />
-      </div>
-      <input type="text" onChange={e => dispatch(changeSearchProductName(e.target.value))} value={searchProductName} />
-      {
-        products
-          ?.filter(product => product.title.toLocaleLowerCase().includes(searchProductName.toLocaleLowerCase()))
-          ?.map(product => <div className="tooltip__element">
-            <div className="tooltip_element">
-              <p>{product.title}</p>
-              <Tooltip message={`route to ${product.id}`} position="bottom" />
-            </div>
-          </div>)
-      }
-      <hr />
-      <h1>Products User get</h1>
-      {
-        userCartProduct.map(product => <div >
-          <h6 onClick={() => dispatch(removeProduct(product.id))}> {product.title}</h6>
-          <button onClick={() => dispatch(incrementProductAmount(product.id))}>+</button>
-          <b>{product.amount}</b>
-          <button onClick={() => dispatch(decrementProductAmount(product.id))}>-</button>
-
-        </div>)
-      }
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/page' element={<h1>page</h1>} />
+        <Route path='/page1' element={<h1>page1</h1>} />
+        <Route path='/page2' element={<h1>page2</h1>} />
+      </Routes>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
 import './scss/main.scss'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
