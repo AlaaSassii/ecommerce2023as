0f8623121e81a6800c78ff9479a75f695860e3bd
alaasassii <alaasassijs@gmail.com>
2023-10-19 21:27:34 +0100

    unistall a package

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
+import RateSelction from './components/RateSelction'
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/page' element={<h1>page</h1>} />
-        <Route path='/page1' element={<h1>page1</h1>} />
-        <Route path='/page2' element={<h1>page2</h1>} />
-      </Routes>
+      <RateSelction />
     </>
   )
 }
--- src/components/RateSelction/RateSelction.tsx
+++ src/components/RateSelction/RateSelction.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import ReactStars from 'react-rating-stars-component'
 import './RateSelction.scss'
 const RateSelction = () => {
+    const secondExample = {
+        size: 30,
+        count: 5,
+        color: "black",
+        activeColor: "gold",
+        value: 7.5,
+        a11y: true,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />,
+        onChange: (newValue: number | string) => {
+            console.log(`Example 2: new value is ${newValue}`);
+        }
+    };
     return (
-        <div>RateSelction</div>
+        <div className='rate__selction'>
+            <ReactStars  {...secondExample} />
+        </div>
     )
 }
 
