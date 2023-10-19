57b6d99e1cfa693fdc3946be1412561d0473208e
alaasassii <alaasassijs@gmail.com>
2023-10-19 21:43:08 +0100

    add new things

--- src/components/RateSelction/RateSelction.tsx
+++ src/components/RateSelction/RateSelction.tsx
@@ -1,24 +1,18 @@
-import ReactStars from 'react-rating-stars-component'
+import { Rating } from '@mui/material'
+
 import './RateSelction.scss'
 const RateSelction = () => {
-    const secondExample = {
-        size: 30,
-        count: 5,
-        color: "black",
-        activeColor: "gold",
-        value: 7.5,
-        a11y: true,
-        isHalf: true,
-        emptyIcon: <i className="far fa-star" />,
-        halfIcon: <i className="fa fa-star-half-alt" />,
-        filledIcon: <i className="fa fa-star" />,
-        onChange: (newValue: number | string) => {
-            console.log(`Example 2: new value is ${newValue}`);
-        }
-    };
+
     return (
         <div className='rate__selction'>
-            <ReactStars  {...secondExample} />
+            <Rating
+                name="half-rating"
+                defaultValue={2.5}
+                precision={0.5}
+                onChange={(event, newValue) => {
+                    console.log(newValue);
+                }}
+            />
         </div>
     )
 }
