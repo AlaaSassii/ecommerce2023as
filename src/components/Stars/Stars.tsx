40dfa0669fb142baf86f2b7382b248ec03920d2b
alaasassii <alaasassijs@gmail.com>
2023-10-19 12:42:05 +0100

    update component

--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -2,18 +2,20 @@ import { FC } from 'react'
 import { TbStarHalfFilled, TbStarFilled } from 'react-icons/tb'
 import { generateStarArray } from '../../helper/generateStarArray'
 import './Stars.scss'
+
 type StarsPropsType = {
     rate: number
+    fontSize: string
 }
-const Stars: FC<StarsPropsType> = ({ rate }) => {
+const Stars: FC<StarsPropsType> = ({ rate, fontSize }) => {
     return (
         <div className='product_rate'>
             {
                 generateStarArray(rate)
                     .map((star, index) => {
-                        if (star === 'S') return <TbStarFilled className="star__true" />
-                        if (star === 'H') return <TbStarHalfFilled className="star__true" />
-                        if (star === 'E') return <TbStarFilled className="star__false" />
+                        if (star === 'S') return <TbStarFilled style={{ fontSize }} key={`star__${index}`} className="star__true" />
+                        if (star === 'H') return <TbStarHalfFilled style={{ fontSize }} key={`star__${index}`} className="star__true" />
+                        if (star === 'E') return <TbStarFilled style={{ fontSize }} key={`star__${index}`} className="star__false" />
                     })
             }
         </div>
