ed8d09095124591cefac97e00e20355f8da8e22f
alaasassii <alaasassijs@gmail.com>
2023-10-20 23:28:04 +0100

    add mew styles

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,21 @@
-import Container from "../components/Container"
+import '../scss/Home.scss'
 const Home = () => {
     return (
-        <section className='first__section'>
-            <div className="content">
-                <h1>Welcome to Our E-Commerce website</h1>
-                <p>Welcome to Our E-Commerce Website! Discover a world of shopping convenience and endless possibilities.</p>
-                <p>Find the finest products, exclusive deals, and outstanding service. Start exploring and enjoy a remarkable shopping experience!</p>
-                <p>Thank you for choosing us as your preferred online shopping destination. Happy shopping!</p>
-                <div className="buttons">
-                    <button type="button">Browse Products</button>
-                    <button type="button">Shop Now</button>
+        <>
+            <section className='welcome__section'>
+                <div className="content">
+                    <h1>Welcome to Our E-Commerce website</h1>
+                    <p>Welcome to Our E-Commerce Website! Discover a world of shopping convenience and endless possibilities.</p>
+                    <p>Find the finest products, exclusive deals, and outstanding service. Start exploring and enjoy a remarkable shopping experience!</p>
+                    <p>Thank you for choosing us as your preferred online shopping destination. Happy shopping!</p>
+                    <div className="buttons">
+                        <button type="button">Browse Products</button>
+                        <button className="shop__btn">Shop Now</button>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
+
     )
 }
 
