1e9a27ceaf5d4f41fd5ecbc218ae648fe02574e5
alaasassii <alaasassijs@gmail.com>
2023-10-20 22:54:05 +0100

    add new things

--- src/routes/UserCart.tsx
+++ src/routes/UserCart.tsx
@@ -2,6 +2,9 @@ import { useAppSelector } from '../hooks/useAppSelector'
 import UserProductCard from '../components/UserProductCard'
 import { Container } from '@mui/material';
 import '../scss/UserCart.scss'
+import { calculateTotalAmount } from '../helper/calculateTotalAmount';
+import { calculateTotalPrice } from '../helper/calculateTotalPrice';
+import { formatNumber } from '../helper/formatNumber';
 
 const UserCart = () => {
     const { userCartProduct } = useAppSelector(state => state.userCart)
@@ -26,8 +29,18 @@ const UserCart = () => {
                 </div>
                 <div className='total__Shoping__cart'>
                     <div>
-                        <div></div>
+                        <div><p>{calculateTotalAmount(userCartProduct)} item(s)</p><p>${formatNumber(calculateTotalPrice(userCartProduct))}</p></div>
+                        <div><p>Shiping</p><p>$26.00</p></div>
+                        <a>Have a promo Code?</a>
                     </div>
+                    <hr />
+                    <div>
+                        <div><p>Total: </p> <p>${formatNumber(calculateTotalPrice(userCartProduct) + 26)}</p></div>
+                        <div><p>Taxes</p><p>$30.00</p></div>
+                        <div><p>Total</p><p>${formatNumber(calculateTotalPrice(userCartProduct) + 256)}</p></div>
+                    </div>
+                    <hr />
+                    <button>Checkout</button>
                 </div>
             </div>
         </Container>
