be2087bce1cf04dbcdefadb4c52731e0026970e0
alaasassii <alaasassijs@gmail.com>
2023-10-20 10:06:12 +0100

    add style for commit

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,7 +1,7 @@
 import Container from "../components/Container"
 import ProductCard from "../components/ProductCard"
 import useGetAllProducts from "../hooks/useGetAllProducts"
-
+import '../scss/Products.scss'
 const Products = ({ }) => {
     const { products, loading, error, } = useGetAllProducts()
     return (
@@ -16,18 +16,20 @@ const Products = ({ }) => {
                             ?
                             <h1>Loading...</h1>
                             :
-                            products.map((p) =>
-                                <ProductCard
-                                    category={p.category}
-                                    id={p.id}
-                                    image={p.image}
-                                    price={p.price}
-                                    product={p}
-                                    rate={p.rating.rate}
-                                    rateCount={p.rating.count}
-                                    title={p.title}
-                                    key={`product__${p.id}`}
-                                />)
+                            <div className="products__contaienr">
+                                {products.map((p) =>
+                                    <ProductCard
+                                        category={p.category}
+                                        id={p.id}
+                                        image={p.image}
+                                        price={p.price}
+                                        product={p}
+                                        rate={p.rating.rate}
+                                        rateCount={p.rating.count}
+                                        title={p.title}
+                                        key={`product__${p.id}`}
+                                    />)}
+                            </div>
 
                 }
             </Container>
