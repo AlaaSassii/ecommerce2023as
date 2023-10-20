c3a242664a1ddb22669bab16019c434d00f78fc8
alaasassii <alaasassijs@gmail.com>
2023-10-20 23:09:22 +0100

    add new selection for home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import Container from "../components/Container"
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='first__section'>
+            <div className="content">
+                <h1>Welcome to Our E-Commerce website</h1>
+                <p>Welcome to Our E-Commerce Website! Discover a world of shopping convenience and endless possibilities.</p>
+                <p>Find the finest products, exclusive deals, and outstanding service. Start exploring and enjoy a remarkable shopping experience!</p>
+                <p>Thank you for choosing us as your preferred online shopping destination. Happy shopping!</p>
+                <div className="buttons">
+                    <button type="button">Browse Products</button>
+                    <button type="button">Shop Now</button>
+                </div>
+            </div>
+        </section>
     )
 }
 
