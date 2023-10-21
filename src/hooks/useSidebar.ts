85564eb9bb6f87ca44600d85edfcef59a109f7d5
alaasassii <alaasassijs@gmail.com>
2023-10-21 10:28:44 +0100

    add logic

--- src/hooks/useSidebar.ts
+++ src/hooks/useSidebar.ts
@@ -1,16 +1,31 @@
+import { useEffect } from 'react'
 import { useDispatch } from "react-redux"
 import { useAppSelector } from "./useAppSelector";
 import { toggleFilterProductSidebar, toggleUserProductSidebar } from '../redux/sidebarsSlice'
+import useWindowWidth from "./useWindowSize";
+import { sidebarProductsFilterEnum } from '../enum/sidebarProductFilter';
 
 export const useSideBar = () => {
     const dispatch = useDispatch();
     const { showUserProductsSideBar, showFilterProductSidebar } = useAppSelector(state => state.sidebars);
+    const windowWidth = useWindowWidth();
+
     const toggleFilterProductSidebarFunction = () => {
         dispatch(toggleFilterProductSidebar())
     }
     const toggleUserProductSidebarFunction = () => {
         dispatch(toggleUserProductSidebar())
     }
+
+    useEffect(() => {
+        if (windowWidth <= 991 && showFilterProductSidebar) {
+            toggleFilterProductSidebarFunction()
+        } else {
+            if (windowWidth >= 991 && showFilterProductSidebar === sidebarProductsFilterEnum.hide) {
+                toggleFilterProductSidebarFunction()
+            }
+        }
+    }, [windowWidth])
     return { showUserProductsSideBar, showFilterProductSidebar, toggleFilterProductSidebarFunction, toggleUserProductSidebarFunction }
 
 }
