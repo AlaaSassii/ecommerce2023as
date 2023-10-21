71ba05dfe11ed1d8ec626ad63176b3fc93333a3f
alaasassii <alaasassijs@gmail.com>
2023-10-21 09:23:41 +0100

    add new things

--- src/components/ErrorNoProduct/ErrorNoProduct.tsx
+++ src/components/ErrorNoProduct/ErrorNoProduct.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { FC } from 'react';
 import './ErrorNoProduct.scss';
 import noProductFound from '../../assets/no-products-found.png'
 
-const ErrorNoProduct = () => {
+type ErrorNoProductType = {
+    left?: number
+}
+
+const ErrorNoProduct: FC<ErrorNoProductType> = ({ left }) => {
     return (
-        <div>
+        <div style={left ? { left } : {}} className='error__no__product'>
             <h3>there is no product</h3>
             <img src={noProductFound} alt="" />
         </div>
