f466a51a1e2955e88dd59e6b1999434b12d5d7be
alaasassii <alaasassijs@gmail.com>
2023-10-21 11:00:29 +0100

    add new things

--- src/components/ErrorNoProduct/ErrorNoProduct.tsx
+++ src/components/ErrorNoProduct/ErrorNoProduct.tsx
@@ -9,9 +9,14 @@ type ErrorNoProductType = {
     y?: number
 }
 
-const ErrorNoProduct: FC<ErrorNoProductType> = ({ left }) => {
+const ErrorNoProduct: FC<ErrorNoProductType> = ({ left, top, x, y }) => {
+    const cutomErrorProductStyle = {
+        left: left || '50%',
+        top: top || '50%',
+        transform: `translate(${x || 0}, ${y || 0})`
+    };
     return (
-        <div style={left ? { left: `${left}%` } : {}} className='error__no__product' >
+        <div style={cutomErrorProductStyle} className='error__no__product' >
             <h3>there is no product</h3>
             <img src={noProductFound} alt="" />
         </div >
